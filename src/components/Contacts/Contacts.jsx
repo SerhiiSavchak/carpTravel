6879d74e739963109ce70d3f821ad44813e424ec
X-RAY <intelekt200012@gmail.com>
2023-10-04 18:58:52 +0300

    add fix contacts section

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -8,51 +8,77 @@ export const Contacts = () => {
         <h2 className="contacts-title">
           Contact <span className="contacts-title-span">us</span>
         </h2>
-        <ul className="contacts-list">
-          <li className="contacts-item">
-            <a className="contacts-link" href="/">
-              +38 (098) 12 34 567
-            </a>
-            <a className="contacts-link" href="/">
-              +38 (073) 12 34 567
-            </a>
+        <div className="contacts-main-wrap">
+          <div className="contacts-top-wrap">
+            <ul className="contacts-top-list">
+              <li className="contacts-top-item">
+                <a className="contacts-link top" href="/">
+                  +38 (098) 12 34 567
+                </a>
+              </li>
+              <li className="contacts-top-item">
+                <a className="contacts-link top" href="/">
+                  +38 (073) 12 34 567
+                </a>
+              </li>
+            </ul>
             <h3 className="contacts-subtitle">Phone number</h3>
-          </li>
-          <li className=" contacts-item">
+          </div>
+          <div className="contacts-wrap">
             <a className="contacts-link" href="/">
               support@carptravel.com
             </a>
             <h3 className="contacts-subtitle">E-mail</h3>
-          </li>
-          <li className=" contacts-item">
-            <a className="contacts-link" href="/">
-              facebook
-            </a>
-            <a className="contacts-link" href="/">
-              instagram
-            </a>
-            <a className="contacts-link" href="/">
-              youtube
-            </a>
-            <a className="contacts-link" href="/">
-              tiktok
-            </a>
-            <h3 className="contacts-subtitle">Follow us</h3>
-          </li>
-        </ul>
+          </div>
+          <div className="contacts-bottom-wrap">
+            <h3 className="contacts-subtitle bottom">Follow us</h3>
+            <ul className="contacts-bottom-list">
+              <li className="contacts-bottom-item">
+                <a className="contacts-link bottom" href="/">
+                  facebook
+                </a>
+              </li>
+              <li className="contacts-bottom-item">
+                <a className="contacts-link bottom" href="/">
+                  instagram
+                </a>
+              </li>
+              <li className="contacts-bottom-item">
+                <a className="contacts-link bottom" href="/">
+                  youtube
+                </a>
+              </li>
+              <li className="contacts-bottom-item">
+                <a className="contacts-link bottom" href="/">
+                  tiktok
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <form className="contacts-form">
-          <label className="contacts-label">
-            Full name
-            <input className="contacts-input" />
-          </label>
-          <label className="contacts-label">
-            E-mail
-            <input className="contacts-input" />
-          </label>
-          <label className="contacts-label">
-            Message
-            <textarea className="contacts-input textarea" />
-          </label>
+          <div className="contacts-form-wrap">
+            <label className="contacts-label">
+              Full name
+              <input
+                type="text"
+                placeholder="John Smith"
+                className="contacts-input"
+              />
+            </label>
+            <label className="contacts-label">
+              E-mail
+              <input
+                type="email"
+                placeholder="johnsmith@email.com"
+                className="contacts-input"
+              />
+            </label>
+            <label className="contacts-label textarea">
+              Message
+              <textarea className="contacts-input textarea" />
+            </label>
+          </div>
           <button className="contacts-btn" type="submit">
             send
           </button>
