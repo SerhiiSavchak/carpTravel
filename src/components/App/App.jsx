69ba94de72fcf1408734e370ef4bac79e1f57825
X-RAY <intelekt200012@gmail.com>
2023-10-07 15:30:09 +0300

    add fix to preload

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import { Preload } from 'components/Preload/Preload';
 import { Header } from 'components/Header/Header';
 import { Hero } from 'components/Hero/Hero';
 import { About } from 'components/About/About';
@@ -10,6 +11,7 @@ import { Contacts } from 'components/Contacts/Contacts';
 export const App = () => {
   return (
     <>
+      <Preload />
       <Header />
       <Hero />
       <About />
@@ -20,21 +22,3 @@ export const App = () => {
     </>
   );
 };
-
-//  const imgs = [
-//    '../../img/bg/services-section/bg-services-1-mobile.webp',
-//    '../../img/bg/services-section/bg-services-1-tablet.webp',
-//    '../../img/bg/services-section/bg-services-1-desc.webp',
-//    '../../img/bg/services-section/bg-services-2-mobile.webp',
-//    '../../img/bg/services-section/bg-services-2-tablet.webp',
-//    '../../img/bg/services-section/bg-services-2-desc.webp',
-//    '../../img/bg/services-section/bg-services-3-mobile.webp',
-//    '../../img/bg/services-section/bg-services-3-tablet.webp',
-//    '../../img/bg/services-section/bg-services-3-desc.webp',
-//    '../../img/bg/services-section/bg-services-4-mobile.webp',
-//    '../../img/bg/services-section/bg-services-4-tablet.webp',
-//    '../../img/bg/services-section/bg-services-4-desc.webp',
-//    '../../img/bg/services-section/bg-services-5-mobile.webp',
-//    '../../img/bg/services-section/bg-services-5-tablet.webp',
-//    '../../img/bg/services-section/bg-services-5-desc.webp',
-//  ];
