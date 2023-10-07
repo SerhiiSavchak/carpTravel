c7ce7c36c88b2c8074b6c0878e6a36f2e5793510
X-RAY <intelekt200012@gmail.com>
2023-10-07 15:05:31 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,3 +20,21 @@ export const App = () => {
     </>
   );
 };
+
+//  const imgs = [
+//    '../../img/bg/services-section/bg-services-1-mobile.webp',
+//    '../../img/bg/services-section/bg-services-1-tablet.webp',
+//    '../../img/bg/services-section/bg-services-1-desc.webp',
+//    '../../img/bg/services-section/bg-services-2-mobile.webp',
+//    '../../img/bg/services-section/bg-services-2-tablet.webp',
+//    '../../img/bg/services-section/bg-services-2-desc.webp',
+//    '../../img/bg/services-section/bg-services-3-mobile.webp',
+//    '../../img/bg/services-section/bg-services-3-tablet.webp',
+//    '../../img/bg/services-section/bg-services-3-desc.webp',
+//    '../../img/bg/services-section/bg-services-4-mobile.webp',
+//    '../../img/bg/services-section/bg-services-4-tablet.webp',
+//    '../../img/bg/services-section/bg-services-4-desc.webp',
+//    '../../img/bg/services-section/bg-services-5-mobile.webp',
+//    '../../img/bg/services-section/bg-services-5-tablet.webp',
+//    '../../img/bg/services-section/bg-services-5-desc.webp',
+//  ];
