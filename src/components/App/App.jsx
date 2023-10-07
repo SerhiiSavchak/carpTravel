96cdde1c1cfde8e7bb31e70deedf21a87398212a
X-RAY <intelekt200012@gmail.com>
2023-10-07 14:55:14 +0300

    add cashes images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,17 +6,66 @@ import { Services } from 'components/Services/Services';
 import { Career } from 'components/Career/Career';
 import { Gallery } from 'components/Gallery/Gallery';
 import { Contacts } from 'components/Contacts/Contacts';
+import { useEffect, useState } from 'react';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 export const App = () => {
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const imgs = [
+      '../../img/bg/services-section/bg-services-1-mobile.webp',
+      '../../img/bg/services-section/bg-services-1-tablet.webp',
+      '../../img/bg/services-section/bg-services-1-desc.webp',
+      '../../img/bg/services-section/bg-services-2-mobile.webp',
+      '../../img/bg/services-section/bg-services-2-tablet.webp',
+      '../../img/bg/services-section/bg-services-2-desc.webp',
+      '../../img/bg/services-section/bg-services-3-mobile.webp',
+      '../../img/bg/services-section/bg-services-3-tablet.webp',
+      '../../img/bg/services-section/bg-services-3-desc.webp',
+      '../../img/bg/services-section/bg-services-4-mobile.webp',
+      '../../img/bg/services-section/bg-services-4-tablet.webp',
+      '../../img/bg/services-section/bg-services-4-desc.webp',
+      '../../img/bg/services-section/bg-services-5-mobile.webp',
+      '../../img/bg/services-section/bg-services-5-tablet.webp',
+      '../../img/bg/services-section/bg-services-5-desc.webp',
+    ];
+    const cacheImages = async srcArray => {
+      const promises = await srcArray.map(src => {
+        return new Promise(function (resolve, reject) {
+          const img = new Image();
+          img.src = src;
+          img.onload = resolve();
+          img.onerror = reject();
+        });
+      });
+      await Promise.all(promises);
+      setIsLoading(false);
+    };
+
+    cacheImages(imgs);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Hero />
-      <About />
-      <Services />
-      <Career />
-      <Gallery />
-      <Contacts />
+      {!isloading ? (
+        <>
+          <Header />
+          <Hero />
+          <About />
+          <Services />
+          <Career />
+          <Gallery />
+          <Contacts />
+        </>
+      ) : (
+        <ClipLoader
+          loading={isloading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      )}
     </>
   );
 };
